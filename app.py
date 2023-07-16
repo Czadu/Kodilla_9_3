7f8c52427ecdead436b9d21bebfadc29087ad91a
Czadu <127044087+Czadu@users.noreply.github.com>
2023-07-16 16:49:05 +0200

    app - one to many relationship update

--- app.py
+++ app.py
@@ -2,28 +2,34 @@ from flask import Flask, request, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
-
-
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///C:/Users/Jrgrz/Desktop/Python/Kodilla_learning/Kodilla_13/Kodilla_13_4/LIBRARY fin/library.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-
-#Initialize the database
 db = SQLAlchemy(app)
 
-#Create db model
-class Library(db.Model):
-      id = db.Column(db.Integer, primary_key=True)
-      title = db.Column(db.String(200), nullable=False)
-      year = db.Column(db.String(4), nullable=False)
-      description = db.Column(db.String(500))
-      author = db.Column(db.String(200), nullable=False)
-      borrowed = db.Column(db.Boolean)
-      date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
-      def __repr__(self):
-            return '<Title %r>' % self.id
+class Author(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False, unique=True)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    books = db.relationship('Book', backref='author')
+
+    def __repr__(self):
+        return '<Author %r>' % self.name
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    year = db.Column(db.String(4), nullable=False)
+    description = db.Column(db.String(500))
+    borrowed = db.Column(db.Boolean)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    author_id = db.Column(db.Integer, db.ForeignKey('author.id')) # corrected line
+
+    def __repr__(self):
+        return '<Title %r>' % self.id
 
 
 @app.route("/libraries/", methods=['GET', 'POST'])
@@ -32,10 +38,22 @@ def library_20():
         new_title = request.form['title']
         new_year = request.form['year']
         new_description = request.form['description']
-        new_author = request.form['author']
+        author_name = request.form['author']
         new_borrowed = request.form.get("borrowed", False) == "True"
-        new_book = Library(title=new_title, year=new_year, description=new_description, author=new_author,
-                           borrowed=new_borrowed)
+
+        # Check if the author already exists
+        author = Author.query.filter_by(name=author_name).first()
+
+        # If the author does not exist, create a new author
+        if author is None:
+            author = Author(name=author_name)
+            db.session.add(author)
+            db.session.commit()
+
+        # Now create the new book with the author
+        new_book = Book(title=new_title, year=new_year, description=new_description, 
+                           borrowed=new_borrowed, author_id=author.id)
+
         try:
             db.session.add(new_book)
             db.session.commit()
@@ -43,36 +61,43 @@ def library_20():
         except:
             return 'ERROR'
     else:
-        libraries = Library.query.order_by(Library.date_created).all()
+        libraries = Book.query.order_by(Book.date_created).all()
         return render_template("library.html", libraries=libraries)
+
 
 @app.route("/update/<int:id>", methods=['GET', 'POST'])
 def update(id):
-    library_to_update = Library.query.get_or_404(id)
+    book_to_update = Book.query.get_or_404(id)
     if request.method == "POST":
-        library_to_update.title = request.form['title']
-        library_to_update.year = request.form['year']
-        library_to_update.description = request.form['description']
-        library_to_update.author = request.form['author']
-        library_to_update.borrowed = request.form.get("borrowed", False) == "True"
+        book_to_update.title = request.form['title']
+        book_to_update.year = request.form['year']
+        book_to_update.description = request.form['description']
+        author_name = request.form['author']
+        author = Author.query.filter_by(name=author_name).first()
+        if author is None:
+            author = Author(name=author_name)
+            db.session.add(author)
+            db.session.commit()
+        book_to_update.author_id = author.id
+        book_to_update.borrowed = request.form.get("borrowed", False) == "True"
         try:
             db.session.commit()
             return redirect('/libraries/')
         except:
             return "ERROR"
     else:
-        return render_template("update.html", library_to_update=library_to_update)
+        return render_template("update.html", book_to_update=book_to_update)
 
 @app.route('/delete/<int:id>')
 def delete(id):
-    library_to_delete = Library.query.get_or_404(id)
-
+    book_to_delete = Book.query.get_or_404(id)
     try:
-        db.session.delete(library_to_delete)
+        db.session.delete(book_to_delete)
         db.session.commit()
         return redirect('/libraries/')
     except:
         return "ERROR"
+
 
 
 if __name__ == "__main__":

ace1fce5cc8ce3e6da824440f368af9280e081a2
Czadu <127044087+Czadu@users.noreply.github.com>
2023-07-16 15:43:26 +0200

    Przechodzimy na sqlite

--- forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, BooleanField, IntegerField
-from wtforms.validators import DataRequired, Length, NumberRange
-
-class LibraryForm(FlaskForm):
-    title = StringField('Tytuł', validators=[DataRequired(), Length(max=100)])
-    year = IntegerField('Rok', validators=[DataRequired(), NumberRange(min=0)])
-    description = TextAreaField('Opis', validators=[DataRequired(), Length(max=1000)])
-    done = BooleanField('Przeczytane')
